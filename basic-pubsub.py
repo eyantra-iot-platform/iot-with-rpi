d3b4e7f8cd36100cec42718bfca120d78ef6831d
Omkar Manjrekar <manjrekarom@gmail.com>
2018-06-08 18:19:56 +0530

    Servo and LED control done

--- basic-pubsub.py
+++ basic-pubsub.py
@@ -3,24 +3,29 @@ import time
 import json
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 
+# Details about thing
+THING_ID = "thing22"
+CLIENT_ID = "MyRpi"
+CERTIFICATE_PATH = "./certificates"
+ENDPOINT = "akbmorjah98q5.iot.ap-southeast-1.amazonaws.com"
+
 
 # Change to your topics here
-UPDATE_TOPIC = "$aws/things/thing22/shadow/update"
-DELTA_TOPIC = "$aws/things/thing22/shadow/update/delta"
-CERTIFICATE_PATH = "./certificates"
+UPDATE_TOPIC = "$aws/things/" + THING_ID + "/shadow/update"
+DELTA_TOPIC = "$aws/things/" + THING_ID + "/shadow/update/delta"
+
 ROOT_CA = CERTIFICATE_PATH + "/rootCA.pem"
 PRIVATE_KEY = CERTIFICATE_PATH + "/private.key.pem"
 CERTIFICATE_CRT = CERTIFICATE_PATH + "/certificate.crt.pem"
 
 
 # Configuration for AWS IoT
-myMQTTClient = AWSIoTMQTTClient("myClientID")
-myMQTTClient.configureEndpoint("akbmorjah98q5.iot.ap-southeast-1.amazonaws.com", 8883)
+myMQTTClient = AWSIoTMQTTClient(CLIENT_ID)
+myMQTTClient.configureEndpoint(ENDPOINT, 8883)
 myMQTTClient.configureCredentials(ROOT_CA, PRIVATE_KEY, CERTIFICATE_CRT)
 
 
-# myMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
-# myMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+myMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
 myMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
 myMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
 myMQTTClient.enableMetricsCollection()
@@ -50,8 +55,10 @@ def publish_sensor_data(interval):
 		threading.current_thread().interrupt()
 
 
-def update_thing(client, userdata, message):
+def update_thing_state(client, userdata, message):
 	print(client, userdata, message)
+
+	# Message payload is of type bytes
 	print("Message payload:", dict(message.payload))
 	print("Message topic:", message.topic)
 
@@ -60,4 +67,4 @@ if __name__ == "__main__":
 	t = threading.Thread(target=publish_sensor_data, args = (5,))
 	t.start()
 
-	myMQTTClient.subscribe(DELTA_TOPIC, 0, update_thing)
+	myMQTTClient.subscribe(DELTA_TOPIC, 0, update_thing_state)
